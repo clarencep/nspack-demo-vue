22974f0f4f365ac0759938df9cab65703e8b5333
Clarence <clarence-pan@foxmail.com>
2018-03-15 18:13:28 +0800

    add demo for less

---
+++ src/tests/test-less.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        This is a test for less.
+        <p class="red">
+            If less works, this block will be red.
+        </p>
+        <p class="green">
+            this block will be green.
+        </p>
+        <p class="yellow">
+            this block will be yellow.
+        </p>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                instance: ''
+            }
+        },
+        mounted(){
+            this.instance = new DemoClass("Hello world!")
+        }
+    }
+
+    class DemoClass {
+        constructor(msg){
+            this.message = msg
+        }
+    }
+</script>
+<style lang="less">
+    @import "./test-less.less";
+    @import "./test-less.2.less";
+    @import "./test-less-include-css.css";
+
+    /*this is style tag in the vue file*/
+    body{
+        background: #eee;
+    }
+    div {
+        max-width: 400px;
+        margin: 0 auto;
+        p{
+            background: #fff;
+        }
+    }
+</style>
