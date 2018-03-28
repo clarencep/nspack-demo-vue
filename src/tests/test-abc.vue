18fb418cd714f4d2002439b9f138c7bf6b745638
Clarence <clarence-pan@foxmail.com>
2018-03-28 09:57:42 +0800

    add test-abc.vue

---
+++ src/tests/test-abc.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        test abc
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                instance: ''
+            }
+        },
+        mounted(){
+            this.instance = new DemoClass("Hi world! ccc")
+        }
+    }
+
+    class DemoClass {
+        constructor(msg){
+            this.message = msg
+        }
+    }
+</script>
